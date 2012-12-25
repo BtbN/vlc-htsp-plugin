26885d4e4c77376f28773dbcee7791a5187d8917
BtbN <btbn@btbn.de>
2012-12-25 22:28:51 +0100

    Changes

--- vlc-htsp-plugin.c
+++ vlc-htsp-plugin.c
@@ -23,11 +23,14 @@
 #include <vlc_common.h>
 #include <vlc_plugin.h>
 #include <vlc_demux.h>
+#include <vlc_access.h>
 #include <vlc_charset.h>
 #include <vlc_cpu.h>
 
 int OpenHTSP(vlc_object_t *);
 void CloseHTSP(vlc_object_t *);
+int DemuxHTSP(demux_t *demux);
+int ControlHTSP(demux_t *access, int i_query, va_list args);
 
 vlc_module_begin ()
 	set_shortname( "HTSP Protocol" )
@@ -41,7 +44,7 @@ vlc_module_end ()
 
 struct demux_sys_t
 {
-	int tmp;
+	mtime_t start;
 };
 
 int OpenHTSP(vlc_object_t *obj)
@@ -52,14 +55,57 @@ int OpenHTSP(vlc_object_t *obj)
 	if(unlikely(sys == NULL))
 		return VLC_ENOMEM;
 	demux->p_sys = sys;
-	
 
-	return VLC_EGENERIC;
+	printf("I'm here!\n");
+
+	demux->pf_demux = DemuxHTSP;
+	demux->pf_control = ControlHTSP;
+
+	sys->start = mdate();
+
+	return VLC_SUCCESS;
 }
 
 void CloseHTSP(vlc_object_t *obj)
 {
 	demux_t *demux = (demux_t*)obj;
+	demux_sys_t *sys = demux->p_sys;
 
+	printf("No longer here\n");
+
+	free(sys);
+}
+
+#define DEMUX_EOF 0
+#define DEMUX_OK 1
+#define DEMUX_ERROR -1
+int DemuxHTSP(demux_t *demux)
+{
+	demux_sys_t *sys = demux->p_sys;
+	printf("I'm demuxing!\n");
+
+	return DEMUX_ERROR;
+}
+
+int ControlHTSP(demux_t *demux, int i_query, va_list args)
+{
+	demux_sys_t *sys = demux->p_sys;
+	printf("I'm controling!\n");
+
+	switch(i_query)
+	{
+		case DEMUX_CAN_PAUSE:
+		case DEMUX_CAN_SEEK:
+		case DEMUX_CAN_CONTROL_PACE:
+			*va_arg(args, bool*) = false;
+			return VLC_SUCCESS;
+		case DEMUX_GET_PTS_DELAY:
+			*va_arg(args, int64_t*) = INT64_C(1000) *var_InheritInteger(demux, "live-caching");
+			return VLC_SUCCESS;
+		case DEMUX_GET_TIME:
+			*va_arg(args, int64_t*) = mdate() - sys->start;
+		default:
+			return VLC_EGENERIC;
+	}
 }
 
