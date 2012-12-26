79c95db394328811aebf79ff32e1b04c74dec3a7
BtbN <btbn@btbn.de>
2012-12-26 19:23:50 +0100

    Connecting works

--- vlc-htsp-plugin.cpp
+++ vlc-htsp-plugin.cpp
@@ -25,6 +25,9 @@
 #include <vlc_url.h>
 #include <vlc_network.h>
 
+#include <deque>
+#include <string>
+
 extern "C"
 {
 #include <libhts/sha1.h>
@@ -54,6 +57,32 @@ struct hts_stream
 
 struct demux_sys_t
 {
+	demux_sys_t()
+		:start(0)
+		,netfd(0)
+		,streamCount(0)
+		,stream(0)
+		,host("")
+		,port(0)
+		,username("")
+		,password("")
+		,channelId(0)
+		,sessionId(0)
+		,nextSeqNum(0)
+	{}
+	
+	~demux_sys_t()
+	{
+		for(std::deque<htsmsg_t*>::iterator it = queue.begin(); it != queue.end(); it++)
+			htsmsg_destroy(*it);
+		queue.clear();
+			
+		if(netfd >= 0)
+			net_Close(netfd);
+
+		vlc_UrlClean(&url);
+	}
+
 	mtime_t start;
 
 	int netfd;
@@ -63,16 +92,26 @@ struct demux_sys_t
 
 	vlc_url_t url;
 
-	char *host;
+	std::string host;
 	uint16_t port;
-	char *username;
-	char *password;
+	std::string username;
+	std::string password;
 	int channelId;
 
+	std::string serverName;
+	std::string serverVersion;
+	int32_t protoVersion;
+	
 	int sessionId;
 
 	uint32_t nextSeqNum;
+	std::deque<htsmsg_t*> queue;
 };
+
+std::string htsmsg_get_stdstr(htsmsg_t *m, const char *n)
+{
+	return htsmsg_get_str(m, n);
+}
 
 bool parseURL(demux_t *demux)
 {
@@ -95,8 +134,10 @@ bool parseURL(demux_t *demux)
 	else
 		sys->port = url->i_port;
 
-	sys->username = url->psz_username;
-	sys->password = url->psz_password;
+	if(url->psz_username)
+		sys->username = url->psz_username;
+	if(url->psz_password)
+		sys->password = url->psz_password;
 
 	if(url->psz_path == 0 || *(url->psz_path) == '\0')
 	{
@@ -152,6 +193,13 @@ htsmsg_t * ReadMessage(demux_t *demux)
 	void *buf;
 	uint32_t len;
 	
+	if(sys->queue.size())
+	{
+		htsmsg_t *res = sys->queue.front();
+		sys->queue.pop_front();
+		return res;
+	}
+	
 	if(net_Read(demux, sys->netfd, NULL, &len, sizeof(len), false) != sizeof(len))
 		return 0;
 
@@ -171,58 +219,152 @@ htsmsg_t * ReadMessage(demux_t *demux)
 	return htsmsg_binary_deserialize(buf, len, buf);
 }
 
-htsmsg_t * ReadResult(demux_t *demux, htsmsg_t *m, bool sequence)
+#define MAX_QUEUE_SIZE 1000
+htsmsg_t * ReadResult(demux_t *demux, htsmsg_t *m, bool sequence = true)
 {
 	demux_sys_t *sys = demux->p_sys;
 
+	uint32_t iSequence = 0;
+	if(sequence)
+	{
+		iSequence = HTSPNextSeqNum(sys);
+		htsmsg_add_u32(m, "seq", iSequence);
+	}
+	
 	if(!TransmitMessage(demux, m))
 		return 0;
 	
-	return 0;
+	std::deque<htsmsg_t*> queue;
+	sys->queue.swap(queue);
+	
+	while((m = ReadMessage(demux)))
+	{
+		uint32_t seq;
+		if(!sequence)
+			break;
+		if(!htsmsg_get_u32(m, "seq", &seq) && seq == iSequence)
+			break;
+		
+		queue.push_back(m);
+		if(queue.size() >= MAX_QUEUE_SIZE)
+		{
+			msg_Dbg(demux, "Max queue size reached!");
+			sys->queue.swap(queue);
+			return 0;
+		}
+	}
+	
+	sys->queue.swap(queue);
+	
+	if(!m)
+		return 0;
+	
+	const char *error;
+	if((error = htsmsg_get_str(m, "error")))
+	{
+		msg_Err(demux, "HTSP Error: %s", error);
+		htsmsg_destroy(m);
+		return 0;
+	}
+	uint32_t noaccess;
+	if(!htsmsg_get_u32(m, "noaccess", &noaccess) && noaccess)
+	{
+		msg_Err(demux, "Access Denied");
+		htsmsg_destroy(m);
+		return 0;
+	}
+	
+	return m;
+}
+
+bool ReadSuccess(demux_t *demux, htsmsg_t *m, const std::string &action, bool sequence = true)
+{
+	if((m = ReadResult(demux, m, sequence)) == 0)
+	{
+		msg_Err(demux, "ReadSuccess - failed to %s", action.c_str());
+		return false;
+	}
+	htsmsg_destroy(m);
+	return true;
 }
 
 bool ConnectHTSP(demux_t *demux)
 {
 	demux_sys_t *sys = demux->p_sys;
 
-	sys->netfd = net_ConnectTCP(demux, sys->host, sys->port);
+	sys->netfd = net_ConnectTCP(demux, sys->host.c_str(), sys->port);
 
 	if(sys->netfd < 0)
 		return false;
 
-	htsmsg_t *m, *cap;
-	htsmsg_field_t *f;
+	htsmsg_t *m;
 
 	m = htsmsg_create_map();
 	htsmsg_add_str(m, "method", "hello");
 	htsmsg_add_str(m, "clientname", "VLC media player");
 	htsmsg_add_u32(m, "htspversion", 7);
 
-	if((m = ReadResult(demux, m, true)) == 0)
+	if((m = ReadResult(demux, m)) == 0)
 		return false;
 
-	return true;
+	size_t chall_len;
+	const void * chall;
+		
+	sys->serverName = htsmsg_get_stdstr(m, "servername");
+	sys->serverVersion = htsmsg_get_stdstr(m, "serverversion");
+	htsmsg_get_s32(m, "htspversion", &(sys->protoVersion));
+	htsmsg_get_bin(m, "challenge", &chall, &chall_len);
+	
+	void * lchall = 0;
+	if(chall && chall_len)
+		lchall = memcpy(malloc(chall_len), chall, chall_len);
+	
+	msg_Info(demux, "Connected to HTSP Server %s, version %s, protocol %d", sys->serverName.c_str(), sys->serverVersion.c_str(), sys->protoVersion);
+	
+	htsmsg_destroy(m);
+	
+	if(sys->username.empty())
+		return true;
+	
+	m = htsmsg_create_map();
+	htsmsg_add_str(m, "method"  , "authenticate");
+	htsmsg_add_str(m, "username", sys->username.c_str());
+	
+	if(sys->password != "" && lchall)
+	{
+		msg_Info(demux, "Authenticating as '%s' with a password", sys->username.c_str());
+
+		HTSSHA1 *shactx = (HTSSHA1*)malloc(hts_sha1_size);
+		uint8_t d[20];
+		hts_sha1_init(shactx);
+		hts_sha1_update(shactx, (const uint8_t *)(sys->password.c_str()), sys->password.length());
+		hts_sha1_update(shactx, (const uint8_t *)lchall, chall_len);
+		hts_sha1_final(shactx, d);
+		htsmsg_add_bin(m, "digest", d, 20);
+		free(shactx);
+	}
+	else
+		msg_Info(demux, "Authenticating as '%s' without a password", sys->username.c_str());
+	
+	if(lchall)
+		free(lchall);
+
+	bool res = ReadSuccess(demux, m, "authenticate");
+	if(res)
+		msg_Info(demux, "Successfully authenticated!");
+	else
+		msg_Err(demux, "Authentication failed!");
+	return res;
 }
 
 static int OpenHTSP(vlc_object_t *obj)
 {
 	demux_t *demux = (demux_t*)obj;
 
-	demux_sys_t *sys = (demux_sys_t*)malloc(sizeof(demux_sys_t));
+	demux_sys_t *sys = new demux_sys_t;
 	if(unlikely(sys == NULL))
 		return VLC_ENOMEM;
 	demux->p_sys = sys;
-
-	sys->netfd = -1;
-	sys->streamCount = 0;
-	sys->stream = 0;
-	sys->host = 0;
-	sys->port = 0;
-	sys->username = 0;
-	sys->password = 0;
-	sys->channelId = 0;
-	sys->sessionId = 0;
-	sys->nextSeqNum = 0;
 
 	demux->pf_demux = DemuxHTSP;
 	demux->pf_control = ControlHTSP;
@@ -256,12 +398,7 @@ static void CloseHTSP(vlc_object_t *obj)
 	if(!sys)
 		return;
 
-	if(sys->netfd >= 0)
-		net_Close(sys->netfd);
-
-	vlc_UrlClean(&(sys->url));
-
-	free(sys);
+	delete sys;
 }
 
 #define DEMUX_EOF 0
@@ -270,6 +407,7 @@ static void CloseHTSP(vlc_object_t *obj)
 static int DemuxHTSP(demux_t *demux)
 {
 	demux_sys_t *sys = demux->p_sys;
+	VLC_UNUSED(sys);
 
 	return DEMUX_ERROR;
 }
